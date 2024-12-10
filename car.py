70ec0c4b0bf6c85ff01c2909f64cb567450851f2
donghoon11 <hoon6049@hanyang.ac.kr>
2024-12-11 02:53:46 +0900

    new file:   car.py 	new file:   main.py 	new file:   manipulator.py 	new file:   util.py

---
+++ car.py
@@ -0,0 +1,77 @@
+from util import Context, Config, ConfigObj, ReadData, ControlData
+import time
+import math
+import numpy as np
+
+
+config: Config = Config()
+configobj: ConfigObj = ConfigObj()
+
+
+# get current location of robot
+def get_location(context: Context, youbot_data: ReadData):
+    if context.state_counter == 1:
+        if youbot_data.localization is not None:
+            location = youbot_data.localization
+        else:
+            location = None
+    return location
+
+
+# move car to pick location
+def move_to_pick(context: Context, youbot_data: ReadData):
+    result: bool = False
+    control_data: ControlData = ControlData
+
+    if context.state_counter == 1:
+        # pick location is already defined. (Context.pick_location_id)
+        control_data.control_cb = move_cb
+        control_data.read_lidar = True
+    else:
+        result = True
+
+    return result, control_data
+
+
+# move car to place location
+def move_to_place(context: Context, youbot_data: ReadData):
+    result: bool = False
+    control_data: ControlData = ControlData
+
+    if context.state_counter == 1:
+        # pick location is already defined. (Context.place_location_id)
+        control_data.control_cb = move_cb
+        control_data.read_lidar = True
+    else:
+        result = True
+
+    return result, control_data
+
+
+# move car to base
+def move_to_base(context: Context, youbot_data: ReadData):
+    result: bool = False
+    control_data: ControlData = ControlData
+
+    if context.state_counter == 1:
+        # pick location is already defined. (Context.base)
+        control_data.control_cb = move_cb
+        control_data.read_lidar = True
+    else:
+        result = True
+
+    return result, control_data
+
+
+def move_cb(context: Context, youbot_data: ReadData, control_data: ControlData):
+
+    forwback_vel = control_data.wheels_velocity_el[0]
+    side_vel = control_data.wheels_velocity_el[1]
+    rot_vel = control_data.wheels_velocity_el[2]
+
+    control_data.wheels_velocity = [
+        -forwback_vel - side_vel - rot_vel,
+        -forwback_vel + side_vel - rot_vel,
+        -forwback_vel + side_vel + rot_vel,
+        -forwback_vel - side_vel + rot_vel,
+    ]
